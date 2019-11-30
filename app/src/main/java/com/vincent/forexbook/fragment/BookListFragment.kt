d3bb0ac9d59b34c70fc86c0d15f99057ef386f10
Vincent <eric61011@gmail.com>
2019-11-30 23:17:42 +0800

    Finish display book mock data and trigger click event

--- app/src/main/java/com/vincent/forexbook/fragment/BookListFragment.kt
+++ app/src/main/java/com/vincent/forexbook/fragment/BookListFragment.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.vincent.forexbook.R
+import com.vincent.forexbook.adapter.BookListAdapter
+import com.vincent.forexbook.entity.Bank
+import com.vincent.forexbook.entity.Book
+import com.vincent.forexbook.entity.CurrencyType
+import kotlinx.android.synthetic.main.fragment_book_list.*
+import java.util.*
 
 class BookListFragment : Fragment() {
+
+    private val listBookListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+        val book = listBook.adapter.getItem(position) as Book
+        Toast.makeText(context!!, book.id, Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -16,5 +29,23 @@ class BookListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val books = getDefaultBookMockData()
+        val adapter = listBook.adapter
+        if (adapter == null) {
+            listBook.adapter = BookListAdapter(context!!, books)
+        }
+        listBook.onItemClickListener = listBookListener
+    }
+
+    private fun getDefaultBookMockData(): List<Book> {
+        val books = mutableListOf<Book>()
+
+        for (type in CurrencyType.values()) {
+            books.add(Book(type.iconResource.toString(), type.getTitle(),
+                Bank.FUBON, type, Date()))
+        }
+
+        return books
     }
 }
